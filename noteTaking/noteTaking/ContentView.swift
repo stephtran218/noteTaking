ee81a215b320f1f288d471c04dcf535bb9b49905
StudentAM <studentam@Administrators-MacBook-Pro.local>
2024-04-11 10:25:24 -0700

    4/11

--- noteTaking/noteTaking/ContentView.swift
+++ noteTaking/noteTaking/ContentView.swift
@@ -7,23 +7,37 @@
 
 import SwiftUI
 
+struct Note {
+    var title: String
+    var content: String
+}
+
 struct ContentView: View {
+    
+    @State private var notesArray: [Note] = []
+    
     var body: some View {
         NavigationView {
             ZStack {
                 NavigationLink(
-                    destination: NewNoteView(),
+                    destination: NewNoteView(notesArray: $notesArray),
                     label: {
                         Text("Add Task")
+                            .padding()
+                            .font(.system(size: 28))
                             .foregroundColor(.white)
                             .frame(maxWidth: .infinity)
                             .background(Color.blue)
+                            .edgesIgnoringSafeArea(.bottom)
+                            .offset(y: 40)
                     }
                 )
+                .alignmentGuide(.bottom) { _ in
+                    UIScreen.main.bounds.height - 40
+                }
             }
             .navigationBarTitle("Notes")
         }
-        .padding()
     }
 }
 
--- noteTaking/noteTaking/NewNoteView.swift
+++ noteTaking/noteTaking/NewNoteView.swift
@@ -8,11 +8,31 @@
 import SwiftUI
 
 struct NewNoteView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+    
+    @State private var title: String = ""
+    @State private var noteInput: String = ""
+    @Binding var notesArray: [Note]
+    
+    let lightGray = Color(red: 229/255, green: 229/255, blue: 229/255)
+
+        var body: some View {
+            NavigationView{
+                VStack {
+                    TextField("Title", text: $title)
+                        .padding()
+                        .border(Color.black)
+                        .padding(.horizontal)
+                        .background(Color.gray.opacity(0.1))
+                    TextEditor(text: $noteInput)
+                        RoundedRectangle(cornerRadius: 8)
+                            .foregroundColor(lightGray)
+                }
+                .navigationBarTitle("New Note")
+
+            }
+        }
 }
 
 #Preview {
-    NewNoteView()
+    NewNoteView(notesArray: .constant([]))
 }
